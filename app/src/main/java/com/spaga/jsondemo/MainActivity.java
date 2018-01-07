0668da600e2dfb7f8a70d169655a83ae02dad023
spagabtti <spagabtti@gmail.com>
2018-01-06 17:57:22 -0800

    edit

--- app/src/main/java/com/spaga/jsondemo/MainActivity.java
+++ app/src/main/java/com/spaga/jsondemo/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DownloadTask task = new DownloadTask();
-        task.execute("api.openweathermap.org/data/2.5/weather?q=London,uk");
+        task.execute("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=" +
+                "b6907d289e10d714a6e88b30761fae22");
     }
 }
